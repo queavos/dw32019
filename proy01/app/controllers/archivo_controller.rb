eb98dcd9f888448bd8c1a4d3c18cb4288783f52d
OzBa <queavos.dev@gmail.com>
2019-09-12 21:23:06 -0400

    form editar

--- proy01/app/controllers/archivo_controller.rb
+++ proy01/app/controllers/archivo_controller.rb
@@ -1,14 +1,25 @@
 class ArchivoController < ApplicationController
     skip_before_action :verify_authenticity_token
   def index
+    @personas=Persona.all
   end
+  def new
 
-  def read
+  end
+  def create
     @nombre=params[:nombre]
     @apellido=params[:apellido]
     @perso=Persona.new
     @perso.nombre=params[:nombre]
     @perso.apellido=params[:apellido]
-    @perso.save
+     if   @perso.save
+        redirect_to archivos_index_path
+      else
+        render "new"
+      end
+  end
+  def edit
+    @id=params[:id]
+    @persona=Persona.find(@id)
   end
 end
--- proy01/config/routes.rb
+++ proy01/config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
 #rutas archivo
   get 'archivo/index'
-  post 'archivo/read'
-
+  get 'archivo/new'
+  post 'archivo/create'
+  get 'archivo/edit/:id', to: 'archivo#edit', as: 'archivo_edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
