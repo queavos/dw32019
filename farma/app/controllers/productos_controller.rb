07cebf7bad47f81cfbcceafd9f1b83a18770be67
Ozba <qvsdev@gmail.com>
2019-11-21 19:12:14 -0300

    rails condicion

--- farma/app/controllers/productos_controller.rb
+++ farma/app/controllers/productos_controller.rb
@@ -2,8 +2,16 @@ class ProductosController < ApplicationController
   before_action :authenticate_user!, except: [:delete, :index]
 
   def index
-  @lista= Producto.all
-
+  #@lista= Producto.all
+  #@lista= Producto.where(rubro_id: 2)
+  #cond1= " rubro_id = 1"
+  #cond2= "laboratorio_id = 1 "
+  #cond3= cond1+" and "+cond2
+ #cond3= " descripcion like '%ol' "
+ #cond3 = "laboratorio_id IN (1,4) "
+ cond3 = "laboratorio_id IN (?) ", [1,4]
+  @lista= Producto.where(cond3)
+  #@lista= Producto.where(laboratorio_id: [1,4])
   end
 
   def new
